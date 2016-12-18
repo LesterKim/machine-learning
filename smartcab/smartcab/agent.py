b21520eb88a401ae19f3c4814ee0ccc068e69e51
LesterKim <lkim@post.harvard.edu>
2016-12-17 19:04:44 -0500

    Added inputs['left'] to agent.py and corrected notebook with update report.html

--- smartcab/smartcab/agent.py
+++ smartcab/smartcab/agent.py
@@ -62,7 +62,7 @@ class LearningAgent(Agent):
         ###########
         # Set 'state' as a tuple of relevant data for the agent
         
-        state = (waypoint, inputs['light'], inputs['oncoming'])
+        state = (waypoint, inputs['light'], inputs['oncoming'], inputs['left'])
 
         return state
 
@@ -182,7 +182,7 @@ def run():
     #   learning   - set to True to force the driving agent to use Q-learning
     #    * epsilon - continuous value for the exploration factor, default is 1
     #    * alpha   - continuous value for the learning rate, default is 0.5
-    agent = env.create_agent(LearningAgent, learning=True, alpha=.4)
+    agent = env.create_agent(LearningAgent, learning=True, alpha=.3)
     
     ##############
     # Follow the driving agent
